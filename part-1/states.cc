89d7dddae28aa2505a10dda049ed223262a3085e
Brian Milian <brianmilian@csu.fullerton.edu>
2022-11-28 14:34:52 -0800

    one unittest fails

--- part-1/states.cc
+++ part-1/states.cc
@@ -7,7 +7,8 @@
 // Lab 10-03
 // Partners: @engenies
 //
-// creates a file that outputs a colored canvas of red, green, blue, and yellow
+// this file includes the constructors, getter methods, and methods for the
+// State class
 //
 
 #include "states.h"
@@ -42,97 +43,83 @@ std::vector<std::vector<std::string>> ReadCSV(const std::string& csv_filename,
   return table;
 }
 
+// constructor with parameters
 State::State(const std::string& name, int population, double land_area) {
-  // TODO: complete the definition of this constructor.
-  // Since this is a constructor, it should use a member initialization list
-  // to initialize data members.
-  // When you are done, delete this comment.
   name_ = name;
   population_ = population;
   land_area_ = land_area;
 }
 
+// constructor
 State::State() {
-  // TODO: complete the definition of this constructor.
-  // Since this is a constructor, it should use a member initialization list
-  // to initialize data members.
-  // When you are done, delete this comment.
   name_ = "";
   population_ = 0;
   land_area_ = 0.0;
 }
 
-const std::string& State::Name() const {
-  // TODO: write statements to implement this function, and delete this comment
-  // Hint: This is a simple accessor function. The function definition only
-  // needs to be a single statement.
-  return name_; // TODO: replace this return statement with one that actually works
-}
+// accessor function or getter method
+const std::string& State::Name() const { return name_; }
 
-int State::Population() const {
-  // TODO: write statements to implement this function, and delete this comment
-  // Hint: This is a simple accessor function. The function definition only
-  // needs to be a single statement.
-  return population_; // TODO: replace this return statement with one that actually works
-}
+// accessor function or getter method
+int State::Population() const { return population_; }
 
-double State::LandArea() const {
-  // TODO: write statements to implement this function, and delete this comment
-  // Hint: This is a simple accessor function. The function definition only
-  // needs to be a single statement.
-  return land_area_; // TODO: replace this return statement with one that actually works
-}
+// accessor function or getter method(do we need setter methods for these getter
+// methods?)
+double State::LandArea() const { return land_area_; }
 
+//(is this a method?)
 double State::PopulationDensity() const {
-  // TODO: write statements to implement this function, and delete this comment
-  // Hint: Population density is the population divided by the land area.
-  return double(population_)/land_area_; // TODO: replace this return statement with one that actually works
+  return double(population_) / land_area_;
 }
 
 std::vector<State> ReadStates(const std::string& csv_filename) {
-  // TODO: write statements to implement this function, and delete this comment
-  // Hint: this function should:
-  //  - create an empty vector of States
-  std::vector<std::string> states;
-  int columns;
-  //  - call ReadCSV to create a 2D vector of strings
+  // empty vector of States to add State objects
+  //(does it matter if I include curly braces{}?)
+  std::vector<State> states;
+  //(what do I initialize columns to?)
+  //(if I don't need to initialize vector 'states', why do I need to initialize
+  // 'columns'?) (if I initialize 'columns', does it affect what my vector
+  // 'table' will look like?)
+  int columns = 0;
+  // creating 2D vector 'table' by calling function ReadCSV()
   std::vector<std::vector<std::string>> table{ReadCSV(csv_filename, columns)};
-  //  - loop through each row of the CSV
-  //    - skip the first row that contains a header
-  std::string this_row{""};
-  std::string name{""};
-  std::string population{""};
-  std::string land_area{""};
-  //int populaton_num{0};
-  //double land_area_num{0.0};
 
+  // string variable name to store the name of a state from table
+  std::string name;
+  // int variable population to store the population size of a state from table
+  int population{0};
+  // double variable land_area to store the land_area size of a state from table
+  double land_area{0.0};
+
+  // looping through the CSV file to get the name, population size, and
+  // land_area size from all states in table
   for (int row = 0; row < table.size(); row++) {
-    for (int column = 0; column < table.at(row).size(); column++) {
-      this_row = table.at(row).at(column);
-      if (this_row == (table.at(0).at(column))) {
-        continue;
-      }
-      // get the name (as a string) from column 0
-      if (this_row == table.at(row).at(0)) {
-        name = table.at(row).at(0);
-      // get the population (as a string) from column 2
-      // convert the string to an int with std::stoi
-      } if (this_row == table.at(row).at(2)) {
-        population = table.at(row).at(2);
-        int population_num = std::stoi(population);
-      // get the land area (as a string) from column 47
-      // convert the string to a double with std::stod
-      } if (this_row == table.at(row).at(47)) {
-        land_area = table.at(row).at(47);
-        double land_area_num = std::stod(land_area);
-      }
-      if ((row > 2) && (this_row == table.at(row).at(0))) {
-        State new_state(name, populaton_num, land_area_num);
+    // vector variable 'this_row' stores a row from table
+    std::vector<std::string> this_row = table.at(row);
+    // skip row zero
+    if (row == 0) {
+      continue;
+    }
+    for (int column = 0; column < this_row.size(); column++) {
+      // string varriable 'this cell' stores a cell from table
+      std::string this_cell = this_row.at(column);
+
+      // get state name
+      if (column == 0) {
+        name = this_cell;
+        // get population size
+      } else if (column == 2) {
+        population = std::stoi(this_cell);
+        // get land_area size
+      } else if (column == 47) {
+        land_area = std::stod(this_cell);
       }
     }
+    // new object 'new_state'
+    State new_state(name, population, land_area);
+    // adding new_state to states vector
+    states.push_back(new_state);
   }
-  //      - create a State object with the name, population, land area
-  //      - add the State to your vector with push_back
-
-  return std::vector<State>(); // TODO: replace this return statement with one that actually works
+  // returning vector 'states' that includes state objects
+  return states;
 }
